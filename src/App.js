c430b3633d4a804abbcec897d7fcfbc393057290
starlord <starlord@xhiftcorp.com>
2019-04-12 16:11:40 +0900

    styled component 작성

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import styled, { createGlobalStyle } from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <GlobalStyle />
+        <Button success>Hello</Button>
+        <Button danger>Hello</Button>
+        <Anchor href="https://www.google.com/">Go to Google</Anchor>
+      </Container>
     );
   }
 }
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    paddingh:0;
+    margin:0;
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width:100%;
+  background-color:papayawhip ;
+`;
+
+const Button = styled.button`
+  border-radius:50px;
+  padding:5px;
+  min-width: 200px;
+  min-height: 41px;
+  color:white;
+  text-align: center
+  font-size: 20px;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline:none;
+  }
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color:${props => props.danger ? "#e74c3c" : "#2ecc71" }
+`;
+
+const Anchor = styled (Button.withComponent("a")) `
+  text-decoration : none;
+`;
 
 export default App;
