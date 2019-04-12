d109b0ebf1128aff874fc74239e69155db178dad
starlord <starlord@xhiftcorp.com>
2019-04-12 17:08:01 +0900

    nesing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react';
 // import "./App.css";
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes, ThemeProvider } from "styled-components";
+import theme from "./theme"
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <GlobalStyle />
-        <Input placeholder="Hello" />
-        {/* <Button success >Hello</Button>
-        <Button danger rotationTime={"5"}>Hello</Button> */}
-        {/* <Anchor href="https://www.google.com/">Go to Google</Anchor> */}
-      </Container>
+      // 리덕스 provider 처럼 전체를 감싸서 어디서든지 접근할 수 있게.
+      <ThemeProvider theme = {theme} >
+        <Container>
+          <GlobalStyle />
+            <Form />
+          {/* <Input placeholder="Hello" /> */}
+          {/* <Button success >Hello</Button>
+          <Button danger rotationTime={"5"}>Hello</Button> */}
+          {/* <Anchor href="https://www.google.com/">Go to Google</Anchor> */}
+        </Container>
+      </ThemeProvider>
     );
   }
 }
 
 
-//mixin 은 css그룹 내가 그룹화하고싶은 여러장소에서 쓰고싶은 것
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius:10px;
-  padding: 20px;
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
+
+
+const Card = styled.div`
+  background-color:white;
 `;
 
-// attr 을 수정할 수있다.
-const Input = styled.input.attrs({
-  required: true  
-})`
-  border:none;
-  border-radius: 5px;
-  ${awesomeCard};
+const Button = styled.button`
+  border-radius:30px;
+  padding:25px  15px;
+  background-color: ${props => props.theme.successColor};
 `;
+
+//mixin 은 css그룹 내가 그룹화하고싶은 여러장소에서 쓰고싶은 것
+// const awesomeCard = css`
+//   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+//   background-color: white;
+//   border-radius:10px;
+//   padding: 20px;
+// `;
+
+// attr 을 수정할 수있다.
+// const Input = styled.input.attrs({
+//   required: true  
+// })`
+//   border:none;
+//   border-radius: 5px;
+//   ${awesomeCard};
+// `;
 
 const Container = styled.div`
   height: 100vh;
   width:100%;
-  background-color:papayawhip ;
+  background-color:#FFEFD5 ;
+
+  // nesting - sass에서 보통하던 스타일안의 스타일
+  ${Card}{
+    background-color:blue
+  }
 `;
 
 
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+// 디자이너랑 일할때 색상이랑 색상팔레트로 작업한다 보통
+// 예를 들면 모든 보더는 파란색이어야한다.
+// 전체프로덕트에서 쓰다가 파란색보다 갑자기 빨간색이 낫다며 바꿔달라고할땐?
+// theme 화시켜서 쓴다.
+
+const theme = {
+     mainColor:"#FFEFD5",
+     dangerColor:"#e74c3c",
+     successColor:"#2ecc71"
+}
+
+export default theme;
